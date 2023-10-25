2155cf6fbc1a5275e6c7c918b6b8b0680580453a
Phung Duc Dung <ducdungphung2002@gmail.com>
2023-10-25 21:26:30 +0700

    Comment

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -1,6 +1,50 @@
+import commentModel from "../models/commentModel.js";
+import { io } from "../index.js";
+class CommentController {
+  async createComment(req, res) {
+    try {
+      const { content, recipeId } = req.body;
+      const userId = req.user._id;
 
-class CommentController{
+      const newComment = new commentModel({
+        owner: userId,
+        content,
+        recipeId,
+      });
 
+      const savedComment = await newComment.save();
+      io.emit("commentAdded", savedComment);
+
+      return res.status(201).json({
+        success: true,
+        data: savedComment,
+      });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({
+        success: false,
+        error: error.message,
+      });
+    }
+  }
+
+  async getCommentsForRecipe(req, res) {
+    try {
+      const { recipeId } = req.params;
+
+      const comments = await commentModel.find({ recipeId });
+
+      return res.status(200).json({
+        success: true,
+        data: comments,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        error: error.message,
+      });
+    }
+  }
 }
 
-export default new CommentController;
+export default new CommentController();
--- src/index.js
+++ src/index.js
@@ -8,10 +8,19 @@ import { authenticateToken, facebookAuthen, googleAuthen } from './authen.js'
 import passport from 'passport'
 import cookieParser from 'cookie-parser';
 import session from 'express-session'
+import { Server } from "socket.io";
+import http from "http";
 
 const { KEY_SESSION } = process.env;
 const app = express();
 
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+export { io };
 
 config();
 app.use(express.json());
@@ -122,6 +131,6 @@ connect(process.env.DATABASE_URL, {
 const PORT = process.env.PORT || 5000;
 
 router(app);
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log("Connected to post 5000");
 })
